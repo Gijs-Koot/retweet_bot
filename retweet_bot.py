74bc713d7cd011172f7111844818ce293703ce46
Gijs Koot <gijs.koot@tno.nl>
2013-11-26 22:30:26 +0100

    come on

---
+++ retweet_bot.py
@@ -0,0 +1,66 @@
+import tweepy
+from pprint import pprint 
+from tweepy.streaming import StreamListener, Stream
+
+import simplejson as json
+with open('config.json') as f:
+	config = json.load(f)
+
+def get_main_identity_auth():
+
+	consumer_key = 'L0DJV29U9yT8EpoJibqwg'
+	consumer_secret = 'zsqoS1NU40vrFfHZiUWy4USNnrtTksgr9yhsD1SQ'
+
+	access_token = '1091901422-gjnIbtd1bfStyEBKFfpGfvtH9BOhkWbGiQNQGQx' 
+	access_secret = '12WGFJuogIzTxOzqZ1pFD6fT3j7usZnj1sflAZIT8oJ8f'
+
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_secret)
+
+	return auth
+
+def get_client_auths():
+
+	return [get_main_identity_auth()]
+
+def get_client_names():
+
+	return ['tw_explorer']
+
+def is_retweetable_tweet(status):
+
+	if 'user' in status and not status['user']['screen_name'] in get_client_names(): return True
+	return False	
+
+def condition(status):
+	
+	if is_retweetable_tweet(status) and	config["condition"] in status["text"]: return True
+	return False
+
+def retweet_by_all(status):
+
+	for auth in get_client_auths():
+		
+		api = tweepy.API(auth)
+		api.retweet(status["id"])
+		print 'i will retweet', status["id"], status["text"]
+
+class StdOutListener(StreamListener):
+    
+    # listens to stream and will start retweet_by_all if condition
+    def on_data(self, data):
+        
+        status = json.loads(data)
+        if condition(status):
+	        retweet_by_all(status)
+	
+    def on_error(self, status):
+        print status
+
+if __name__ == "__main__":
+
+	# starts main loop
+	auth = get_main_identity_auth()
+	api = tweepy.API(auth)
+	stream = Stream(auth, StdOutListener(StreamListener))
+	stream.filter(config["listen"])
